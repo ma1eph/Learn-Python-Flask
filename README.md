7703b7b43841c63bd92d9154d6b429c285eba612
Akihiro Ushizaka <lounge.act.2011@gmail.com>
2019-02-17 20:41:08 +0900

    Add Flask application

---
+++ README.md
@@ -0,0 +1,29 @@
+# MAC OS / LINUX
+
+1. Create python virtual environment:
+```
+python -m virtualenv env
+```
+2. Activate it:
+```
+source env/bin/activate
+```
+3. Install dependencies:
+```
+source -m pip install -r requirements.txt
+```
+4. Run the application:
+```
+python main.py
+```
+5. In the web browser, enter the following address:
+```
+http://localhost:8080/
+```
+
+# Run unit tests
+
+1. Run the unittest:
+```
+python -m unittest discover
+```
---
+++ test_main.py
@@ -0,0 +1,33 @@
+#!/bin/python
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+import main
+
+
+class MainTest(unittest.TestCase):
+
+    def test_index(self):
+        main.app.testing = True
+        client = main.app.test_client()
+
+        r = client.get('/')
+        self.assertEqual(200, r.status_code)
+        self.assertIn('Hello World', r.data.decode('utf-8'))
+        self.assertEqual(400, r.status_code)
+
+
+if __name__ == '__main__':
+    unittest.main()
